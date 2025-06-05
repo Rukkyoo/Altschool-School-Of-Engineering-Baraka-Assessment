f3033ff27d6134d3363ce0728254535ca6fdcd51
Michael Omonedo <rukkyomonedo@gmail.com>
2025-06-05 16:37:02 +0100

    fixed js bug

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+// This script handles the functionality of a simple calculator
+// It allows users to input numbers and operators, perform calculations, and clear the display.
 let displayPanel = document.getElementById("calculator-display");
 const calculatorKeyPad = document.getElementById("calculator-keypad");
 const calculatorOperator = document.getElementById("calculator-operator");
 const calculatorEqual = document.getElementById("calculator-operator-equal");
-const calculatorClear = document.getElementById("calculator-keypad-clear")
+const calculatorClear = document.getElementById("calculator-keypad-clear");
+let calculatorDisplayAnswer = document.getElementById(
+  "calculator-display-answer"
+);
 
-let numbers = "";
-let operatorSym = "";
+let numbers = ""; // Where the numbers and operators are being stored on the console
 
+// Event listener for the calculator buttons
 calculatorKeyPad.addEventListener("click", (event) => {
   if (event.target.classList.contains("calculator-keypad-num")) {
     const keyPadValue = event.target.textContent;
@@ -17,6 +22,8 @@ calculatorKeyPad.addEventListener("click", (event) => {
   }
 });
 
+
+// Event listener for the operator buttons
 calculatorOperator.addEventListener("click", (event) => {
   if (event.target.classList.contains("calculator-operator-sym")) {
     const operatorValue = event.target.textContent;
@@ -27,18 +34,21 @@ calculatorOperator.addEventListener("click", (event) => {
   }
 });
 
+
+// Event listener for the equal button (calculation operation)
 calculatorEqual.addEventListener("click", () => {
   const expression = numbers;
   const result = eval(expression);
   console.log("Result:", result);
   console.log("Equal button clicked");
-  let calculatorDisplayAnswer = document.getElementById(
-    "calculator-display-answer"
-  );
   calculatorDisplayAnswer.textContent = result;
+  displayPanel.innerHTML = expression + " = " + result;
 });
 
+
+// Event listener for the clear button (clear the display and reset)
 calculatorClear.addEventListener("click", () => {
-    displayPanel.innerHTML = "";
-    numbers = "";
+  calculatorDisplayAnswer.textContent = "";
+  displayPanel.innerHTML = "";
+  numbers = "";
 });
